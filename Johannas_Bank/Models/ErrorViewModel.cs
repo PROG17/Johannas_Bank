04f3adc38ec777de76ddc9adf140aee39d8aaf7b
johanna åkerström <johanna_akerstrom@hotmail.com>
2018-11-07 14:07:55 +0100

    Add project files.

---
+++ Johannas_Bank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Johannas_Bank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
